6469da80533fd98060e0548851d6a9b831536ea2
LenarGilmanov <lagilmanovla@gmail.com>
2024-12-10 23:50:14 +0300

    first commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../app');
+it('responds with Hello, World!', async () => {
+  const response = await request(app).get('/'); 
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('Hello,Node World!\n');
+});
+
+
+// app.test.js
+
+const concatStrings = require('./app');
+
+test('concatenates strings', () => {
+    expect(concatStrings('Hello, ', 'world!')).toBe('Hello, world!');
+    expect(concatStrings('Foo', 'Bar', 'Baz')).toBe('FooBarBaz');
+});
---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+
+function concatStrings(...strings) {
+    return strings.join('');
+}
+
+module.exports = concatStrings;
+
